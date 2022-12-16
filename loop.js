dd458aa4f189cf53e3724a0aadabece6bce27342
⁕ VДIDДS LДMДИДUSKДS ⁕ <vaidaslamanauskas@gmail.com>
2022-12-16 20:15:10 +0200

    Update loop.js

--- loop.js
+++ loop.js
@@ -9,12 +9,9 @@ setInterval(function() {
   // remove current favicon
   if(document.querySelector("link[rel='icon']") !== null)
      document.querySelector("link[rel='icon']").remove();
-      
-  if(document.querySelector("link[rel='shortcut icon']") !== null)
-     document.querySelector("link[rel='shortcut icon']").remove();
 
   // add new favicon image
-  document.querySelector("head").insertAdjacentHTML('beforeend', '<link rel="icon" href="' + favicons[counter] + '" type="image/gif">');
+  document.querySelector("head").insertAdjacentHTML('beforeend', '<link rel="icon" href="' + favicons[counter] + '">');
 
   // If last image then goto first image
   // Else go to next image    
